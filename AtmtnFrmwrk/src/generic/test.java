37dffc5a05fd5266e6fe94e5c4a1e1e7d4f08f26
Apoorvaishwarya <Apoorvaishwarya@Lenovo-PC>
2018-02-02 06:03:58 +0530

    Third Commit

---
+++ AtmtnFrmwrk/src/generic/test.java
@@ -0,0 +1,14 @@
+package generic;
+
+public class test {
+
+	public static void main(String[] args) {
+	
+		int i=10;
+		System.out.println(i);
+		Long l=new Long(i);
+		System.out.println(l);
+		
+	}
+	
+}
